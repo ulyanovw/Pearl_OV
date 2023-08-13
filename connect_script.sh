6761b6dc5cb75562095371591cfe8dc78227c189
ulyanovw <82198040+ulyanovw@users.noreply.github.com>
2023-08-14 01:18:11 +0300

    Delete connect_script.sh

--- connect_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -f "$path_to_conf/ccd/$common_name" ] && grep -q "ifconfig-push" "$path_to_conf/ccd/$common_name"; then
-    exit 0
-else
-    echo "ifconfig-push $ifconfig_pool_remote_ip 255.255.0.0" >> "$path_to_conf/ccd/$common_name"
-fi
