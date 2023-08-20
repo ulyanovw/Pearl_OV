3de6548d0523cdd62e3bb20c25376e642d1593cd
ulyanovw <82198040+ulyanovw@users.noreply.github.com>
2023-08-20 16:50:43 +0300

    Update connect_script.sh

--- connect_script.sh
+++ connect_script.sh
@@ -4,12 +4,12 @@ CLIENT_NAME="$common_name"
 CLIENT_LOCAL_IP="$ifconfig_pool_remote_ip"
 
 connect() {
+    update_connection_status true
     if [ -f "$path_to_conf/ccd/$CLIENT_NAME" ] && grep -q "ifconfig-push" "$path_to_conf/ccd/$CLIENT_NAME"; then
         exit 0
     else
         echo "ifconfig-push $CLIENT_LOCAL_IP 255.255.0.0" >> "$path_to_conf/ccd/$CLIENT_NAME"
     fi
-    update_connection_status true
 }
 
 disconnect() {
